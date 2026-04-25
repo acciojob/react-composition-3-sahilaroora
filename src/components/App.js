dc30186bc4e6a3f7a7871f2d42c681d93c71645d
sahilaroora <sarora203@gmail.com>
2026-04-25 14:26:25 +0530

    your message

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,54 @@
+import React, { useState } from "react";
+import "./styles.css"; // optional for styling
 
-import React from "react";
-import './../styles/App.css';
+function Tooltip({ text, children }) {
+  const [show, setShow] = useState(false);
 
-const App = () => {
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div
+      className="tooltip"
+      onMouseEnter={() => setShow(true)}
+      onMouseLeave={() => setShow(false)}
+      style={{ display: "inline-block", position: "relative" }}
+    >
+      {children}
+
+      {show && (
+        <span
+          className="tooltiptext"
+          style={{
+            visibility: "visible",
+            backgroundColor: "black",
+            color: "#fff",
+            textAlign: "center",
+            borderRadius: "5px",
+            padding: "5px",
+            position: "absolute",
+            bottom: "125%",
+            left: "50%",
+            transform: "translateX(-50%)",
+            whiteSpace: "nowrap"
+          }}
+        >
+          {text}
+        </span>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default function App() {
+  return (
+    <div style={{ padding: "50px" }}>
+      <Tooltip text="Hello Tooltip!">
+        <button>Hover me</button>
+      </Tooltip>
+
+      <br /><br />
+
+      <Tooltip text="This is a paragraph tooltip">
+        <p>Hover over this text</p>
+      </Tooltip>
+    </div>
+  );
+}
