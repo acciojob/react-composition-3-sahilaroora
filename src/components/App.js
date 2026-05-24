b6b18b4b2f49c90c8a72e6df03f2cfeaeed23735
sahilaroora <sarora203@gmail.com>
2026-05-24 11:02:51 +0530

    Completed React Router User Profiles assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,38 @@
-import React, { useState } from "react";
-import "./styles.css"; // optional for styling
+import React, { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
 
-function Tooltip({ text, children }) {
-  const [show, setShow] = useState(false);
+function App() {
+  const { id } = useParams();
+
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      });
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div
-      className="tooltip"
-      onMouseEnter={() => setShow(true)}
-      onMouseLeave={() => setShow(false)}
-      style={{ display: "inline-block", position: "relative" }}
-    >
-      {children}
+    <div>
+      <p>Name: {user.name}</p>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
 
-      {show && (
-        <span
-          className="tooltiptext"
-          style={{
-            visibility: "visible",
-            backgroundColor: "black",
-            color: "#fff",
-            textAlign: "center",
-            borderRadius: "5px",
-            padding: "5px",
-            position: "absolute",
-            bottom: "125%",
-            left: "50%",
-            transform: "translateX(-50%)",
-            whiteSpace: "nowrap"
-          }}
-        >
-          {text}
-        </span>
-      )}
+      <Link to="/">Back</Link>
     </div>
   );
 }
 
-export default function App() {
-  return (
-    <div style={{ padding: "50px" }}>
-      <Tooltip text="Hello Tooltip!">
-        <button>Hover me</button>
-      </Tooltip>
-
-      <br /><br />
-
-      <Tooltip text="This is a paragraph tooltip">
-        <p>Hover over this text</p>
-      </Tooltip>
-    </div>
-  );
-}
+export default App;
